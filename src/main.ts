9f046a336c9f9fa28766520451bc40797f79a6b7
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-27 16:53:30 +0100

    fix: handle proper shutdown

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,33 @@
-import { getNewListingsAndPushToTelegram } from "./utils/queries";
+import { Telegraf } from "telegraf";
+import { appShutdown } from "./utils/appShutdown";
+import { runTelegramBot } from "./utils/telegram/tg.bot";
 
-async function main(argv: string[]) {
-  await getNewListingsAndPushToTelegram(parseInt(argv[2]));
+let bot: Telegraf;
+
+async function main(argv: string[], shutdownSignal: EventTarget) {
+  console.log("Starting app with:", argv[2]);
+
+  // Process stays alive
+  bot = await runTelegramBot();
+
+  await new Promise<void>((resolve, reject) => {
+    shutdownSignal.addEventListener("abort", () => {
+      console.log("Shutdown signal received in main(). Cleaning up...");
+      resolve();
+    });
+  });
+
 }
 
-main(process.argv).catch((err) => {
-  console.error(err, `Uncaught error: ${err.message}`);
-  process.exitCode = 1;
+const abortCtlr = new AbortController();
+const signal = abortCtlr.signal;
+
+const shutdown = appShutdown((reason: string) => {
+  abortCtlr.abort(); // Signal to main
+  bot.stop(reason);
 });
 
-process
-  .on("unhandledRejection", (why) => {
-    console.error(why ?? {}, `Unhandled rejection: ${(why as Error)?.message}`);
-  })
-  .on("SIGTERM", () => {
-    console.error("SIGTERM signal received: closing HTTP server");
-  })
-  .on("uncaughtException", (err) => {
-    console.error(err, `Uncaught Exception: ${err.message}`);
-    process.exitCode = 1;
-  });
+main(process.argv, signal).catch((err) => {
+  console.error(err, `Uncaught error: ${err.message}`);
+  shutdown("main() threw an error", err);
+});
