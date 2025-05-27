ef9fee96b971582239f316ebea20ceab810b11c8
amochuko <7204868+amochuko@users.noreply.github.com>
2025-05-27 23:35:40 +0100

    fix: add args

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,11 +1,13 @@
-export function formatUSDCompact(amount: number): string {
+export function formatUSDCompact(
+  amount: number,
+  notation: "compact" | "standard" | "scientific" | "engineering" = "standard",
+  maximumFractionDigits = 2
+): string {
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
-    notation: "compact",
+    notation,
     compactDisplay: "short",
-    maximumFractionDigits: 2,
+    maximumFractionDigits,
   }).format(amount);
 }
-  
-  
